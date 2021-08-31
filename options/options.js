e0e2c97ad90acb39388ae25ed8c57894c35eff9f
Crowdbotics <team@crowdbotics.com>
2021-08-31 09:49:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tstdanieldjart_dev_22880Identifier",
+  url: "https://tstdanieldjart_dev_22880Identifier.botics.co",
+  api: "https://tstdanieldjart_dev_22880Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
